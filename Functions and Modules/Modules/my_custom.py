ba63a40725b8c6b341d9233e9cc90bf41fef9fd4
Mohit Uniyal <mohituniyal2010@gmail.com>
2021-08-30 12:59:22 +0530

    first commit

---
+++ Functions and Modules/Modules/my_custom.py
@@ -0,0 +1,9 @@
+def greetings(name):
+    return "Hello "+ name
+
+# print("I will have some functionality in this module")
+
+print("Custome Module Name:", __name__)
+
+if __name__ == '__main__':
+    print("This is a very Confidential Code")
---
+++ Functions and Modules/Modules/project.py
@@ -0,0 +1,6 @@
+import my_custom
+
+res = my_custom.greetings("Mohit")
+print(res)
+
+print("Project Module Name:", __name__)
